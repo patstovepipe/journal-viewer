a8eb92f36fc12a5abc150d46ad7e7f679746cc09
patstovepipe <pstobbe85@gmail.com>
2016-05-04 16:47:51 -0700

    Add action for API, removed paginate

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,25 +1,44 @@
 class JournalController < ApplicationController
   def index
-    debugger
-  	@journalrows = Array.new
-    @journalrows = @journalrows.paginate(:per_page => 10) 
+    #debugger
+  	
+    @journalrows = Array.new
+    
+    # todo get paginate working
+    #@journalrows = @journalrows.paginate(:per_page => 10) 
+
   end
 
   def search
   	#debugger
-  	
-    @journalrows = Array.new
 
-    for row in %x(journalctl -b).split("\n")
-      journalrow = JournalRow.new
-      journalrow.text = row      
-      @journalrows.push(journalrow)
-    end
-  	
-    @journalrows = @journalrows.paginate(:per_page => 10)
+    @journalrows = data(params[:flags])
 
     render 'index'
   end
+
+  # for API call
+  def journalctl
+    @journalrows = data(params[:flags])
+    
+    render json: @journalrows
+  end
+
+  private def data(flags)
+    cmd = "journalctl"
+
+    if !flags.nil? && flags != ""
+      cmd = cmd + " " + flags
+    end
+
+    @journalrows = Array.new
+    for row in %x[ #{cmd} ].split("\n")    
+      journalrow = JournalRow.new
+      journalrow.text = row
+      @journalrows.push(journalrow)
+    end
+
+    return @journalrows
+  end
 end
 
-#https://gitter.im/noelbk/OpenHack
